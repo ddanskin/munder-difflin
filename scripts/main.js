a611d813fdb7fc8da46509f1f769b2b4caed2ebd
Danielle Danskin <danielle.danskin@gmail.com>
2018-03-14 17:41:59 -0400

    start Munder Difflin website mock up

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+
+document.getElementById("submit-email-btn").addEventListener('click', function() {
+    window.alert("One of our knowledgable representatives will email you soon!");
+});
+
+document.addEventListener('scroll', function() {
+
+});

5e747586f0707307043c9ae38aeecaf813f5a75c
Danielle Danskin <danielle.danskin@gmail.com>
2018-03-14 23:34:20 -0400

    create functioning slideshow

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,14 @@
+let allSlides = document.querySelectorAll('#slideshow .slides');
+let curSlide = 0;
+let slideInt = setInterval(nextSlide, 3000);
+
+function nextSlide() {
+    if(curSlide < allSlides.length - 1) {
+        allSlides[curSlide].className = 'slides';
+    }
+    curSlide = curSlide + 1;
+    allSlides[curSlide].className = 'slides show';
+}
 
 document.getElementById("submit-email-btn").addEventListener('click', function() {
     window.alert("One of our knowledgable representatives will email you soon!");

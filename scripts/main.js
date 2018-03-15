3a1891e794b086e52449560dacc0b62f900e4bf7
Danielle Danskin <danielle.danskin@gmail.com>
2018-03-14 22:16:48 -0400

    edit functionality and layout

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,11 @@ document.getElementById("submit-email-btn").addEventListener('click', function()
     window.alert("One of our knowledgable representatives will email you soon!");
 });
 
-document.addEventListener('scroll', function() {
-
+window.addEventListener('scroll', function(event) {
+    const topOfScreen = window.pageYOffset;
+    if (topOfScreen > 2200) {
+        let current = document.getElementById("reviews-box");
+        current.className = "fadeIn";
+    }
 });
+
